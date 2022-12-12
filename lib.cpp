0d0f7f268a376a1a82a3ceb001014d170407be63
farinaema <117036663+farinaema@users.noreply.github.com>
2022-12-12 08:30:43 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+float max( float m[], int dimensione ){
+
+    int i;
+    float c;
+    c=m[0];
+    for (i=0; i<dimensione; i++){
+
+        if(m[i]>=c){
+            c=m[i];
+        }
+    }
+
+    return c;
+
+}
